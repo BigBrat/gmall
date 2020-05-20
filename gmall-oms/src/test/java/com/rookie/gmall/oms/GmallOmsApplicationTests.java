34181abd0215a56114ef0e9ed4192f481e28f189
caizongming <czm_zkf@163.com>
2020-05-20 13:44:25 +0800

    '初始化'

---
+++ gmall-oms/src/test/java/com/rookie/gmall/oms/GmallOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.gmall.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallOmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gmall-wms/src/test/java/com/rookie/gmall/wms/GmallWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookie.gmall.wms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
